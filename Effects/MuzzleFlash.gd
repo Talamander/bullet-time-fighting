dada11bbf858c943dc3bbacad8bc5289cbb69a79
Talamander <toxcrewproductions@gmail.com>
2020-09-16 14:26:34 -0500

    Re-implemented Multiplayer

--- Effects/MuzzleFlash.gd
+++ Effects/MuzzleFlash.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 func _ready():
 	$Particles2D.emitting = true
+	set_as_toplevel(true)
 
+func _process(delta):
+	if Global.bullet_time == true:
+		$Particles2D.speed_scale = .5
+	if Global.bullet_time == false:
+		$Particles2D.speed_scale = 1
 
 func _on_decayTimer_timeout():
 	queue_free()
--- Parent Classes/Projectile.gd
+++ Parent Classes/Projectile.gd
@@ -1,13 +1,28 @@
-extends KinematicBody2D
+extends Area2D
+
+puppet var puppet_motion = Vector2.ZERO
 
 var velocity = Vector2.ZERO
-export(int) var speed = 1000
+var speed = 1500
+
+func _ready():
+	set_as_toplevel(true)
+
 
 func _process(delta):
-	position += velocity * delta
+	if Global.bullet_time == true:
+		speed = 500
+		$Particles2D.speed_scale = .5
+	else:
+		speed = 1500
+		$Particles2D.speed_scale = 1
+	position += velocity * delta * speed
 	
 
 
 func _on_decayTimer_timeout():
-	queue_free()
+	pass#queue_free()
 
+
+func _on_VisibilityNotifier2D_screen_exited():
+	pass#queue_free()
